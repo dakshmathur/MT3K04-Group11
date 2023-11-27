4050b1625875539b8b68026fdd3271d50e1e7c57
sounda2 <arminsound2003@gmail.com>
2023-11-26 20:42:21 -0500

    Clean Up
    After pulling new code that included egrams the merge caused a small visual issue which was cleaned up

--- DCM/main.py
+++ DCM/main.py
@@ -376,10 +376,8 @@ def dashboard_state():
     button_logout.place(relx=0.80, rely=0.35, anchor='w')
 
     # Egram buttom
-    button_egram = tk.Button(frame, text="Egram", command=lambda: eg.init())
-    button_egram.place(relx=0.80, rely=0.45, anchor='w')
     button_egram = tk.Button(frame, text="Egram", command=lambda: eg.main())
-    button_egram.place(relx=0.25, rely=0.4, anchor='center')
+    button_egram.place(relx=0.80, rely=0.44, anchor='w')
 
 # Define the clear entire window
 def clear_frame(fr):
